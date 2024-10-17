2ae082d50984abbd35c524b6c7fe69d1e7606495
YejinShin <devnana222@gmail.cim>
2024-10-17 20:14:28 +0900

    build : 스웨거 적용 & 스웨거 결과 첨부

--- hhplus0011/src/main/java/com/tdd/ecommerce/product/presentation/ProductStockController.java
+++ hhplus0011/src/main/java/com/tdd/ecommerce/product/presentation/ProductStockController.java
@@ -6,6 +6,7 @@ import com.tdd.ecommerce.common.model.CommonApiResponse;
 import com.tdd.ecommerce.product.application.ProductService;
 import com.tdd.ecommerce.product.application.ProductServiceResponse;
 import com.tdd.ecommerce.product.domain.ProductInfoDto;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,6 +16,11 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.*;
 
+@Tag(
+        name = "상품 관리 시스템",
+        description = "상품 번호로 상품을 조회할 수 있습니다. " +
+                "pthVariable로 productId가 없으면 재고가 있는 전체 상품을 조회합니다."
+)
 @RestController
 @RequestMapping("/products")
 public class ProductStockController {
