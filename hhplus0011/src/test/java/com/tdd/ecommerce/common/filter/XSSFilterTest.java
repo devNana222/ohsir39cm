5f3138990779fcb20a3acb24dfd572f80e309152
YejinShin <devnana222@gmail.cim>
2024-10-23 22:21:03 +0900

    test : XSSFilterTest2
    mockMvc로 테스트가 안되던 부분 수정하였습니다.
    
    **MockHttpServletRequest** / **MockHttpServletResponse** / **FilterChain** 을 이용하여 스크립트를 파라미터로 담았을 때 변경되는지 확인했습니다.

--- hhplus0011/src/test/java/com/tdd/ecommerce/common/filter/XSSFilterTest.java
+++ hhplus0011/src/test/java/com/tdd/ecommerce/common/filter/XSSFilterTest.java
@@ -1,40 +1,45 @@
 package com.tdd.ecommerce.common.filter;
 
-import com.tdd.ecommerce.common.config.FilterConfig;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
-import org.springframework.http.MediaType;
-import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
 
-import static org.hamcrest.Matchers.containsString;
-import static org.hamcrest.Matchers.not;
-import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
-import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.patch;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import java.io.IOException;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.mock;
 
-@SpringBootTest
-@AutoConfigureMockMvc
-@Import(FilterConfig.class)
 class XSSFilterTest {
-    @Autowired
-    private MockMvc mockMvc;
+
+    private XSSFilter sut = new XSSFilter();
 
     @Test
-    @DisplayName("🟢스크립트를 보내면 컨텐츠에는 보낸 스크립트가 포함되면 안된다.")
-    void testXSSDoFilter_SUCCESS() throws Exception {
-        mockMvc.perform(patch("/customers/1/balance/charge")
-                      //  .content("comment", "<script>alert('XSS');</script>"))
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content("{ \"comment\": \"<script>alert('XSS');</script>\", \"balance\": 1000 }"))
-                .andExpect(status().is4xxClientError())
-                .andExpect(content().string(not(containsString("<script>alert('XSS');</script>"))))
-                .andExpect(content().string(not(containsString("<script>"))))
-                .andExpect(content().string(containsString("& lt;")));
-    }
+    @DisplayName("XSS 필터가 스크립트를 제거한다.")
+    void testXssFilter() throws IOException, ServletException {
+        //given
+        String header = "<script>alert('XSSHeader');</script>";
+        String param = "<script>alert('XSSParam');</script>";
 
+        MockHttpServletRequest request = new MockHttpServletRequest();
+        request.setRequestURI("");
+        request.setMethod("GET");
+        request.addHeader("Header", header);
+        request.addParameter("param", param);
+
+        MockHttpServletResponse response = new MockHttpServletResponse();
+        FilterChain filterChain = mock(FilterChain.class);
+
+        //when
+        XSSRequestWrapper wrappedRequest = new XSSRequestWrapper(request);
+
+        sut.doFilter(request, response, filterChain);
+
+        //then
+        String filteredContent = wrappedRequest.getParameter("param");
+        assertFalse(filteredContent.contains("<script>"));
+        assertTrue(filteredContent.contains("& lt;& gt;alert& #40;"));
+    }
 }
