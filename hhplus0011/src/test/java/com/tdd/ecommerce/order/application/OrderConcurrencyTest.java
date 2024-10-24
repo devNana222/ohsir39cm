1b6f62a1aba9c323eb5e7f48c0aaa0fbc638f57a
YejinShin <devnana222@gmail.cim>
2024-10-24 21:29:03 +0900

    Step10 : WIL & 동시성 테스트 #19

--- hhplus0011/src/test/java/com/tdd/ecommerce/order/application/OrderConcurrencyTest.java
+++ hhplus0011/src/test/java/com/tdd/ecommerce/order/application/OrderConcurrencyTest.java
@@ -1,14 +1,13 @@
 package com.tdd.ecommerce.order.application;
 
 import com.tdd.ecommerce.common.exception.BusinessException;
-import com.tdd.ecommerce.customer.domain.CustomerRepository;
 import com.tdd.ecommerce.customer.domain.Customer;
-import com.tdd.ecommerce.order.domain.OrderRepository;
+import com.tdd.ecommerce.customer.domain.CustomerRepository;
 import com.tdd.ecommerce.order.domain.Order;
 import com.tdd.ecommerce.order.domain.OrderProduct;
+import com.tdd.ecommerce.order.domain.OrderRepository;
 import com.tdd.ecommerce.product.domain.ProductInventoryRepository;
 import com.tdd.ecommerce.product.domain.ProductRepository;
-
 import com.tdd.ecommerce.product.domain.entity.Product;
 import com.tdd.ecommerce.product.domain.entity.ProductInventory;
 import org.junit.jupiter.api.DisplayName;
